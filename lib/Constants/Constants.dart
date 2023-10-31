1d688065967fd5630d69aa9c3581f9ad3a455355
Keval Gajjar <keval.gajjar@plutustec.com>
2023-11-01 00:17:31 +0530

    Commit for the changes

--- lib/Constants/Constants.dart
+++ lib/Constants/Constants.dart
@@ -13,3 +13,10 @@ const kAPIPath = "/api/v1/emp";
 const kURILogin = "https://inside2.plutustec.com/api/v1/emp/login";
 
 const txtOTPSent = "OTP sent successfully to your registered mobile number!";
+// Login Validation messages
+const errEmptyEmail = 'Please enter the email';
+const errInvalidEmail = 'Please enter the valid email';
+const errEmptyPassword = 'Please enter the password';
+const errInvalidPassword = 'Please enter the valid password';
+RegExp regExp = RegExp(
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
